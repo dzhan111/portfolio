5fb4aeb36f8251ad673a8a726874ab9dcdde4ebf
dzhan111 <zhandavid4@gmail.com>
2024-08-07 14:54:50 -0400

    batman

--- data/index.ts
+++ data/index.ts
@@ -20,7 +20,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "This globe is really cool",
+    title: "Spin the globe",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -45,35 +45,20 @@ export const projects = [
   {
     id: 1,
     title: "Portfolio",
-    des: "A delicious website to showcase my portfolio using NextJS",
+    des: "A delicious website to showcase my portfolio using NextJS. Styled with Aceternity UI animation library",
     img: "/p1.svg",
     iconLists: ["/re.svg", "/next.svg", "/ts.svg", "/fm.svg"],
     link: "/",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    title: "DavidDocs",
+    des: "A realtime document manager and collaboration web app. Styled with shadcn UI library.",
     img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "david-docs.vercel.app",
   },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
-  },
+  
 ];
 
 export const testimonials = [
